697dbf70167253f4fd0a202e31acf15961988a2e
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-02 14:40:36 +0100

    Bonus1 & Bonus2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 // dichiarazione variabili-costanti
 const text3 = "Fizz"
 const text5 = "Buzz"
+const boxEl = document.querySelector(".box");
 
 
 // ciclo e condizioni sui 100 numeri
 for (let i = 1; i <= 100; i++) {
+    let text = "";
+    let bkgColor = "";
+
+
     if (i % 3 === 0 && i % 5 === 0) {
+
         console.log(text3 + text5);
+        text = text3 + text5;
+        bkgColor = "bkg_FizzBuzz";
     } else if (i % 5 === 0) {
+
         console.log(text5);
+        text = text5;
+        bkgColor = "bkg_Buzz";
     } else if (i % 3 === 0) {
+    
         console.log(text3);
+        text = text3;
+        bkgColor = "bkg_Fizz";
     } else {
+
         console.log(`${i}`);
+        text = i;
+        bkgColor = "bkg_number";
     }
+
+    boxEl.innerHTML += `<div class="square ${bkgColor}">${text}</div>` 
 }
